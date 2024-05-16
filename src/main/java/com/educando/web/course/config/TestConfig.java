137c00582fd005ec29438abfbf15d00f7ce4885e
Fabio <fassil0203@gmail.com>
2024-05-15 21:11:37 -0300

    Orderstatus enum

--- src/main/java/com/educando/web/course/config/TestConfig.java
+++ src/main/java/com/educando/web/course/config/TestConfig.java
@@ -10,17 +10,17 @@ import org.springframework.context.annotation.Profile;
 
 import com.educando.web.course.entities.Order;
 import com.educando.web.course.entities.User;
+import com.educando.web.course.entities.enums.OrderStatus;
 import com.educando.web.course.repositories.OrderRepository;
 import com.educando.web.course.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
-public class TestConfig implements CommandLineRunner{
-	
+public class TestConfig implements CommandLineRunner {
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
 
@@ -33,16 +33,14 @@ public class TestConfig implements CommandLineRunner{
 		User u5 = new User(null, "Lisa Simpsom", "lisasimpson@gmail.com", "977776666", "123456");
 		User u6 = new User(null, "Meg Simpson", "megsimpson@gmail.com", "977777777", "123456");
 		User u7 = new User(null, "Dog Simpson", "dogsimpson@gmail.com", "977778888", "123456");
-		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
-		
-		userRepository.saveAllAndFlush(Arrays.asList(u1,u2,u3,u4,u5,u6,u7));
-		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
-		
+
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+
+		userRepository.saveAllAndFlush(Arrays.asList(u1, u2, u3, u4, u5, u6, u7));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
 	}
-	
-	
-	
+
 }

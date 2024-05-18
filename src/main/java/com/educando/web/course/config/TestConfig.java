1d6adf39bdb480781bbdbcfece0d6b6884f84c97
Fabio <fassil0203@gmail.com>
2024-05-18 17:02:25 -0300

    entidade Product

--- src/main/java/com/educando/web/course/config/TestConfig.java
+++ src/main/java/com/educando/web/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.educando.web.course.entities.Category;
 import com.educando.web.course.entities.Order;
+import com.educando.web.course.entities.Product;
 import com.educando.web.course.entities.User;
 import com.educando.web.course.entities.enums.OrderStatus;
 import com.educando.web.course.repositories.CategoryRepository;
 import com.educando.web.course.repositories.OrderRepository;
+import com.educando.web.course.repositories.ProductRepository;
 import com.educando.web.course.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -39,10 +44,16 @@ public class TestConfig implements CommandLineRunner {
 		Category cat5 = new Category(null, "Bombs");
 		Category cat6 = new Category(null, "Tomadas");
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
-		
-		
+				
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		User u3 = new User(null, "Aderaldo tobias", "aderaldotob@gmail.com", "988887896", "123456");
@@ -56,7 +67,7 @@ public class TestConfig implements CommandLineRunner {
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
 		
-		userRepository.saveAll(Arrays.asList(u1, u2));
+		userRepository.saveAll(Arrays.asList(u1, u2, u3,u4,u5,u6,u7));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	
 

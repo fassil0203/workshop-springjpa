394594785f70cd69ae283da67fbe0616c05ecfd0
Fabio <fassil0203@gmail.com>
2024-05-28 22:03:52 -0300

    User update

--- src/main/java/com/educando/web/course/resources/UserResource.java
+++ src/main/java/com/educando/web/course/resources/UserResource.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriComponents.UriTemplateVariables;
 
 import com.educando.web.course.entities.User;
 import com.educando.web.course.services.UserService;
@@ -45,13 +45,18 @@ public class UserResource {
 		return ResponseEntity.created(uri).body(obj);
 
 	}
-	
-	@DeleteMapping(value ="/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Long  id){
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
-		
-	}
-	
 
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+
+	}
 }
